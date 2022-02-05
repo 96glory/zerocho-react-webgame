4aeabc585798c082231b4970d81f0da21f3f4d79
96glory <click_syk@naver.com>
2022-02-05 22:30:01 +0900

    2-10. 끝말잇기 Hooks로 전환하기

---
+++ 2. 끝말잇기/WordRelay.jsx
@@ -0,0 +1,43 @@
+const React = require('react');
+const { useState, useRef } = React;
+
+const WordRelay = () => {
+  const [word, setWord] = useState('영광');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (value && value[0] === word[word.length - 1]) {
+      setResult((prevResult) => {
+        return '정답';
+      });
+      setWord((prevWord) => {
+        return value;
+      });
+      setValue('');
+    } else {
+      setResult('오답');
+      setValue('');
+    }
+    inputRef.current.focus();
+  };
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputRef} onChange={onChangeInput} value={value} />
+        <button>입력</button>
+      </form>
+      <div id="result">{result}</div>
+    </>
+  );
+};
+
+module.exports = WordRelay;
