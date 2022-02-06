26380e7bb86fdd75dcff239bef0bdb072b2c78a0
96glory <click_syk@naver.com>
2022-02-06 15:16:01 +0900

    3-3. 리액트 반복문(key)

--- 3. 숫자야구/BullsAndCows.jsx
+++ 3. 숫자야구/BullsAndCows.jsx
@@ -24,9 +24,19 @@ class BullsAndCows extends Component {
         </form>
         <div>시도 : {this.state.tries.length}</div>
         <ul>
-          {['1', '2', '3', '4', '5'].map((v) => {
-            return <li>{v}</li>;
-          })}
+          {[
+            { fruit: '사과', taste: '맛있다' },
+            { fruit: '바나나', taste: '맛있다' },
+            { fruit: '포도', taste: '씨가 싫다' },
+            { fruit: '귤', taste: '시다' },
+            { fruit: '감', taste: '떫다' },
+          ].map((v, i) => (
+            // key는 고유한 값 (index는 성능 최적화에 문제가 생기므로, 추천하지 않는다.)
+            // 화살표 함수에서 중괄호가 없으면, 곧바로 리턴한다.
+            <li key={i}>
+              <b>{v.fruit}</b> - {v.taste}
+            </li>
+          ))}
         </ul>
       </>
     );
